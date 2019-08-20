e906735f9cc6838dd390c8b33f36f668772f9dac
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-08-20 21:50:28 +0200

    add of percentage unit and dot unit

--- CountOnMe/Controller/CountViewController.swift
+++ CountOnMe/Controller/CountViewController.swift
@@ -10,19 +10,19 @@ import UIKit
 
 class CountViewController: UIViewController {
     @IBOutlet private weak var resultTextView: UITextView!
-    @IBOutlet private weak var displayCalculationTextView: UITextView!
+    @IBOutlet private weak var displayCalculation: UITextView!
     @IBOutlet private var numberButtons: [UIButton]!
     @IBOutlet private var equationButtons: [UIButton]!
     @IBOutlet private weak var  refeshButton: UIButton!
     @IBOutlet private weak var equalButton: UIButton!
 
     var elements: [String] {
-        return displayCalculationTextView.text.split(separator: " ").map { "\($0)" }
+        return displayCalculation.text.split(separator: " ").map { "\($0)" }
     }
 
     // Error check computed variables
     var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/"
+        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/" && elements.last != "%"
     }
 
     var expressionHaveEnoughElement: Bool {
@@ -30,30 +30,11 @@ class CountViewController: UIViewController {
     }
 
     var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/"
+        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/" && elements.last != "%"
     }
 
     var expressionHaveResult: Bool {
         return resultTextView.text.firstIndex(of: "=") != nil
-    }
-
-    // View Life cycles
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    private func equationDisplay(button: UIButton, unit: String) {
-        numberButtons.forEach {$0.isEnabled = true}
-        button.layer.opacity = 0.5
-        if canAddOperator {
-            displayCalculationTextView.text.append(unit)
-        } else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alertVC, animated: true, completion: nil)
-            button.layer.opacity = 1
-        }
     }
 }
 
@@ -61,24 +42,36 @@ private extension CountViewController {
     @IBAction func refresh() {
         equationButtons.forEach {$0.layer.opacity = 1}
         resultTextView.text = ""
-        displayCalculationTextView.text = ""
+        displayCalculation.text = ""
         refeshButton.setTitle("AC", for: .normal)
         numberButtons.forEach {$0.isEnabled = true}
     }
 
     // View actions
-    @IBAction func tappedNumberButton(_ sender: UIButton) {
+    @IBAction func touchDigits(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else { return }
 
-        if displayCalculationTextView.text.count >= 6 {
-            numberButtons.forEach {$0.isEnabled = false}
-        }
-
-        if expressionHaveResult {
-            displayCalculationTextView.text = ""
-        }
-        displayCalculationTextView.text.append(numberText)
         refeshButton.setTitle("C", for: .normal)
+        if numberText == "." && displayCalculation.text.isEmpty {
+            displayCalculation.text.append("0.")
+        } else {
+            if numberText == "." {
+                if let last = elements.last {
+                    if last.contains(".") {
+                        alertPopUp(title: "Decimal Error", message: "A dot has already been entered")
+                    } else {
+                        switch last {
+                        case "+", "-", "/", "x", "%":
+                            displayCalculation.text.append("0.")
+                        default :
+                            displayCalculation.text.append(".")
+                        }
+                    }
+                }
+            } else {
+                displayCalculation.text.append(numberText)
+            }
+        }
     }
 
     @IBAction func tappedAdditionButton(_ sender: UIButton) {
@@ -97,18 +90,22 @@ private extension CountViewController {
         equationDisplay(button: sender, unit: " x ")
     }
 
+    @IBAction func tappedPercentageButton(_ sender: UIButton) {
+        equationDisplay(button: sender, unit: " % ")
+    }
+
     @IBAction func tappedEqualButton(_ sender: UIButton) {
         equationButtons.forEach { $0.layer.opacity = 1 }
         numberButtons.forEach { $0.isEnabled = true }
 
         guard expressionIsCorrect else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zero!", message: "Enter a correct expression!", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
 
         guard expressionHaveEnoughElement else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zero!", message: "Start a new calculation !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
@@ -118,17 +115,34 @@ private extension CountViewController {
 
         // Iterate over operations while an operand still here
         while operationsToReduce.count > 1 {
-            let left = Int(operationsToReduce[0])!
-            let operand = operationsToReduce[1]
-            let right = Int(operationsToReduce[2])!
 
-            let calculate = Calculate(left: left, operand: operand, right: right)
+            let calculate = Calculate(operationsToReduce: operationsToReduce)
             let result = calculate.process()
 
             operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            print(result)
             operationsToReduce.insert("\(result)", at: 0)
         }
         resultTextView.text = "\(operationsToReduce[0])"
-        refeshButton.setTitle("AC", for: .normal)
+    }
+}
+
+private extension CountViewController {
+
+    func alertPopUp(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+
+    func equationDisplay(button: UIButton, unit: String) {
+        numberButtons.forEach { $0.isEnabled = true }
+        button.layer.opacity = 0.5
+        if canAddOperator {
+            displayCalculation.text.append(unit)
+        } else {
+            alertPopUp(title: "Zéro!", message: "Un operateur est déja mis !")
+            button.layer.opacity = 1
+        }
     }
 }
