33dfc43eb89577999bc6088cf6308f4fb820765a
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-08-25 15:37:04 +0200

    unit testing started for Calculate class

--- CountOnMeTests/CountOnMeTests.swift
+++ CountOnMeTests/CountOnMeTests.swift
@@ -11,34 +11,65 @@ import XCTest
 
 class CountOnMeTests: XCTestCase {
 
-    var calculation: Calculate!
+    var testCalculation: Calculate!
 
+    // Reset before each test method in a test case is called.
     override func setUp() {
-        calculation = Calculate()
+        testCalculation = Calculate()
     }
 
-    func testIsExpressionCorrect() {
-        calculation.addNewNumber("9")
-        XCTAssertTrue(calculation.isExpressionCorrect)
+    // Test if expression is correct
+    func testForIsExpressionCorrect() {
+        testCalculation.addNewNumber("9")
+        XCTAssertTrue(testCalculation.isExpressionCorrect)
     }
 
-    func testIsExpressionNotCorrect() {
-        calculation.addNewNumber("")
-        calculation.operators[0] = "+"
-        calculation.elements[0] = ""
-        XCTAssertFalse(calculation.isExpressionCorrect)
+    // Test if espression is not correct
+    func testForIsExpressionNotCorrect() {
+        testCalculation.addNewNumber("")
+        testCalculation.operators[0] = "+"
+        testCalculation.elements[0] = ""
+        XCTAssertFalse(testCalculation.isExpressionCorrect)
     }
 
+    // Test if can add an operator
     func testIfCanAddOperator() {
-        calculation.elements[0] = "4"
-        calculation.elements[0] = "0.0"
-        XCTAssertTrue(calculation.canAddOperator)
+        testCalculation.elements[0] = "4"
+        testCalculation.elements[0] = "0.0"
+        XCTAssertTrue(testCalculation.canAddOperator)
     }
 
+    // Test if cannot add any operator
     func testIfCanNotAddOperator() {
-        calculation.elements[0] = ""
-        calculation.operators[0] = "+"
-        XCTAssertFalse(calculation.canAddOperator)
+        testCalculation.elements[0] = ""
+        testCalculation.operators[0] = "+"
+        XCTAssertFalse(testCalculation.canAddOperator)
     }
 
+    // Test if can add a decimal point
+    func testIfCanAddDecimal() {
+        testCalculation.elements[0] = "1"
+        XCTAssertTrue(testCalculation.canAddDecimal)
+    }
+
+    // Test if cannot add a decimal
+    func testIfCanNotAddDecimal() {
+        testCalculation.elements[0] = ""
+        testCalculation.addDecimal()
+        XCTAssertFalse(testCalculation.canAddDecimal)
+    }
+
+    // Test if can reset calculate properties
+    func testIfCanResetCaculateProperties() {
+        testCalculation.reset()
+        XCTAssertTrue(testCalculation.operators == ["+"])
+        XCTAssertTrue(testCalculation.elements == [""])
+    }
+
+    // Test if cannot reset calculate propeties
+    func testIfCanNotResetCaculateMemory() {
+        testCalculation.reset()
+        XCTAssertFalse(testCalculation.operators == ["+", "-"])
+        XCTAssertFalse(testCalculation.elements == ["1", "4", "8"])
+    }
 }
