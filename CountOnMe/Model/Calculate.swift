b30bdd2459e624d44d065a1241e81316ac300ba4
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-08-29 17:23:56 +0200

    refacto of all the logic for calculate model

--- CountOnMe/Model/Calculate.swift
+++ CountOnMe/Model/Calculate.swift
@@ -10,117 +10,98 @@ import Foundation
 
 class Calculate {
 
-    // MARK: - Properties
-    var elements = [String()]
-    var operators = ["+"]
-
-    /// Check if the last element is a number and can get a calculation result from it
-    var isExpressionCorrect: Bool {
-        if let stringNumber = elements.last {
-            if stringNumber.isEmpty {
-                return false
-            }
-        }
-        return true
+    enum Operator: String {
+        case add = "+"
+        case substract = "-"
+        case multiply = "x"
+        case divide = "÷"
     }
 
-    /// Check if there is already an operator
-    var canAddOperator: Bool {
-        if let last = elements.last {
-            if last.isEmpty {
-                return false
-            }
-        }
-        return true
+    /// Checking if the expression is correct to preform calculation
+    func expressionIsCorrect(elements: [String]) -> Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" && elements.last != "."
     }
 
-    // Check if there is already a dot
-    var canAddDecimal: Bool {
-        if let last = elements.last {
-            if last.contains(".") || last.isEmpty {
-                return false
-            }
-        }
-        return true
+    /// Cheking if there is enough elements in the calcul
+    func expressionHasEnoughElement(elements: [String]) -> Bool {
+        return elements.count >= 3
     }
 
-    // MARK: - CalculatorLogic Methods
-    func addDecimal() {
-        if let last = elements.last {
-            var stringDecimal = last
-            stringDecimal += "."
-            elements[elements.count - 1] = stringDecimal
-        }
+    /// Check if last element contains a decimal point or is empty
+    func isDecimalAddedToLast(elements: [String]) -> Bool {
+        return elements.last?.contains(".") ?? false || elements.last?.isEmpty ?? true
     }
 
-    /// This method is used to add a new number for calculation
-    func addNewNumber(_ newNumber: String) {
-        if let lastNumber = elements.last {
-            var stringNumber = lastNumber
-            stringNumber += "\(newNumber)"
-            elements[elements.count - 1] = stringNumber // Replace last element with the number stored
-        }
-    }
+    /// Checking if operationToReduce countains operator * and /
+    func priorityCalculation(_ operationsToReduce: inout [String]) {
+        while operationsToReduce.contains("x") || operationsToReduce.contains("÷") {
+            if let index = operationsToReduce.firstIndex(where: {$0 == "x" || $0 == "÷"}), let left = Double(operationsToReduce[index - 1]), let right = Double(operationsToReduce[index + 1]) {
+                guard let operand: Operator = Operator(rawValue: operationsToReduce[index]) else { return }
+                var result = 0.0
 
-    // Algorithmic Method for Mathematical Model
-    func calculationLogic() -> Double {
-        var pendingOperand: Double = 0
-        var pendingOperation = ""
-        var total: Double = 0
-
-        for (index, item) in elements.enumerated() {
-            if let number = Double(item) {
-                print("number =  \(number)")
-                switch operators[index] {
-                case "+":
-                    print("bim")
-//                    total = performPendingOperation(operand: pendingOperand, operation: pendingOperation, total: total)
-                    pendingOperand = total
-                    pendingOperation = "+"
-                    total = number
-                case "-":
-                    print("b0m")
-//                    total = performPendingOperation(operand: pendingOperand, operation: pendingOperation, total: total)
-                    pendingOperand = total
-                    pendingOperation = "-"
-                    total = number
-//                    print("1  = \(total)")
-                case "÷":
-                    print("biiiiiim")
-                    total /= number
-                case "×":
-                    print("baaaam")
-                    print("2 = \(total)")
-                    total *= number
-                    print("3 = \(total)")
-                default:
-                    break
+                switch operand {
+                case .multiply:
+                    result = left * right
+                case .divide:
+                    result = left / right
+                default: break
                 }
+
+                checkResult(result: result, index: index - 1, operationsToReduce: &operationsToReduce)
+
+                reducingTheOperation(operationsToReduce: &operationsToReduce, firstIndexToRemove: index + 1, secondIndexToRemove: index)
             }
         }
-        print("4 = \(total)")
-
-        // Perform final pending operation if needed
-        total = performPendingOperation(operand: pendingOperand, operation: pendingOperation, total: total)
-        print("4 = \(total)")
-        return total
     }
 
-    /// This method perform a pending operation ONLY between 2 numbers with operand + or -
-    func performPendingOperation(operand: Double, operation: String, total: Double) -> Double {
-        switch operation {
-        case "+":
-            return operand + total
-        case "-":
-            return operand - total
-        default:
-            return total
+    /// Calculation + and -
+    func nonPriorityCalculation(_ operationsToReduce: inout [String]) {
+        if let left = Double(operationsToReduce[0]), let right = Double(operationsToReduce[2]) {
+            guard let operand: Operator = Operator(rawValue: operationsToReduce[1]) else { return }
+            var result = 0.0
+
+            switch operand {
+            case .add:
+                result = left + right
+            case .substract:
+                result = left - right
+            default: break
+            }
+
+            checkResult(result: result, index: 0, operationsToReduce: &operationsToReduce)
+
+            reducingTheOperation(operationsToReduce: &operationsToReduce, firstIndexToRemove: 2, secondIndexToRemove: 1)
         }
     }
 
-    /// Reset all calculation
-    func reset() {
-        elements = [String()]
-        operators = ["+"]
+    /// Check if the result can be tranform to Int. Replace the first index of the operation by the result.
+    func checkResult(result: Double, index: Int, operationsToReduce: inout [String]) {
+        let intValue = round(result)
+
+        if result.isInfinite || result.isNaN {
+            operationsToReduce[index] = "Error"
+        } else if intValue == result {
+            let resultToInt = Int(result)
+            operationsToReduce[index] = "\(resultToInt)"
+        } else {
+            operationsToReduce[index] = "\(result)"
+        }
+    }
+
+    /// Remove the two index next to operationToReduce.
+    func reducingTheOperation(operationsToReduce: inout [String], firstIndexToRemove: Int, secondIndexToRemove: Int) {
+        operationsToReduce.remove(at: firstIndexToRemove)
+        operationsToReduce.remove(at: secondIndexToRemove)
+    }
+
+    /// Format and calculate the total
+    func calculate(elements: [String]) -> String? {
+        var operationsToReduce = elements
+
+        priorityCalculation(&operationsToReduce)
+        while operationsToReduce.count > 1 {
+            nonPriorityCalculation(&operationsToReduce)
+        }
+        return operationsToReduce.first
     }
 }
